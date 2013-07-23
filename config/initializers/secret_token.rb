4cf67aaedd7eabb4c0e21844b7d2b5598b5a5289
Nate McGuire <nate.mcguire@gmail.com>
2013-07-23 16:41:28 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Natemcguire::Application.config.secret_key_base = '819220697b7559ae495caadb29bcb2cb2d8a08978b9a57e25e0b87a373da67364792b06e0f1a0df4119ef769506dd77a9d73e67fcae7b68ed207cf80ef28dc5a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Natemcguire::Application.config.session_store :cookie_store, key: '_natemcguire_session'
